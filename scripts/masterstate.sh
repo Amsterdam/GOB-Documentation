54830bbe20455410b5d9341cebb63d686d10acc6
vegter <amsjurien@gmail.com>
2020-05-01 15:48:09 +0200

    Update remote branches and show non-standard branches

--- scripts/masterstate.sh
+++ scripts/masterstate.sh
@@ -24,12 +24,25 @@ for REPO in ${REPOS}; do
   GOB_REPO="GOB-${REPO}"
   cd $GOB_REPO
   echo -n "${GOB_REPO} "
-  git remote update origin --prune > /dev/null
+
+  # Update remote repositories
+  git fetch
+
+  # Get diffs between develop and master
   DIFFS=$(git diff --name-status origin/develop origin/master)
   if [ ! -z "${DIFFS}" ]; then
     echo "${RED}Out of date${NC}"
   else
     echo "${GREEN}OK${NC}"
   fi
+
+  # Show any non develop, master or demo branch
+  BRANCHES=$(git ls-remote --heads -q | grep -v -E "develop|master|demo" | awk '{gsub("refs/heads/","",$2); print $2}')
+  if [ ! -z "$BRANCHES" ]; then
+    for BRANCH in $BRANCHES; do
+      echo "  $BRANCH"
+    done
+  fi
+
   cd ..
 done
