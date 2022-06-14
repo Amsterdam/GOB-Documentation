8385121eb37de315510aa7c7da9841f44e79cfc3
Peter Adrichem <Peter.Adrichem@gmail.com>
2022-06-14 16:52:12 +0200

    gob.sh: compose projects instead of containers

--- scripts/docker/gob.sh
+++ scripts/docker/gob.sh
@@ -17,55 +17,56 @@ ALL_REPOS="Workflow Import Prepare Upload API Export Test Message StUF Managemen
 
 REPOS=${REPOS:-${ALL_REPOS}}
 
-# GOB Infrastructure dockers
-INFRA="rabbitmq storage management_database analyse_database"
+# GOB Infrastructure compose project
+INFRA="Infra"
 
 # Change to GOB directory
-cd $SCRIPTDIR/../../..
+cd "$SCRIPTDIR/../../.."
+GOB_BASE_DIR="${PWD}"
 
-stop_docker () {
-    DOCKER=$1
-    docker stop ${DOCKER} > /dev/null 2>&1 || true
-    # docker rm ${DOCKER}
+stop_project () {
+    cd "${GOB_BASE_DIR}/${1}"
+    docker compose ps --services --status=running |
+    while read service
+    do
+        if [ -n "${service}" ]; then
+            echo "  ${service}"
+        fi
+    done
+    docker compose down >/dev/null 2>&1
+    cd - >/dev/null
 }
 
-stop_dockers () {
-    # Stop any running GOB dockers
-    for REPO in ${REPOS}
+stop_projects () {
+    # Stop any running GOB compose project
+    for REPO in ${REPOS} ${INFRA}
     do
-        echo "Stopping docker GOB ${REPO}"
-        DOCKER=$(echo "gob${REPO}" | tr '[:upper:]' '[:lower:]' | tr '-' '_')
-        stop_docker $DOCKER > /dev/null
-    done
-
-    for DOCKER in ${INFRA} prepare_database
-    do
-        echo "Stopping docker GOB ${DOCKER}"
-        stop_docker $DOCKER > /dev/null
+        echo "Stopping ${BOLD_BLACK}GOB ${REPO}${NC}"
+        stop_project "GOB-${REPO}"
     done
 }
 
-show_docker () {
-    GREP=$(docker ps | grep "$1$") || true
-    if [ "$GREP" = "" ]; then
-        echo " ${RED}DOWN${NC}"
-    else
-        echo " ${GREEN}UP${NC}"
-    fi
+list_services () {
+    cd "${GOB_BASE_DIR}/${1}"
+    RUNNING=$(docker compose ps --services --status=running)
+    docker compose config --services |
+    while read service
+    do
+        GREP=$(echo "${RUNNING}" | grep "^${service}$") || true
+        if [ -n "${GREP}" ]; then
+            echo "  ${service} ${GREEN}UP${NC}"
+        else
+            echo "  ${service} ${RED}DOWN${NC}"
+        fi
+    done
+    cd - >/dev/null
 }
 
-list_dockers () {
-    for REPO in ${REPOS}
+show_projects () {
+    for REPO in ${INFRA} ${REPOS}
     do
-        echo -n ${REPO}
-        DOCKER=$(echo "gob${REPO}" | tr '[:upper:]' '[:lower:]' | tr '-' '_')
-        show_docker ${DOCKER}
-    done
-
-    for DOCKER in ${INFRA} prepare_database
-    do
-        echo -n ${DOCKER}
-        show_docker $DOCKER
+        echo "${BOLD_BLACK}GOB ${REPO}${NC}:"
+        list_services "GOB-${REPO}"
     done
 }
 
@@ -74,7 +75,7 @@ start_infra () {
     WAIT_FOR=$2
     TEMPFILE=$(mktemp start_infra.XXXXXX)
     echo -n "Starting ${SERVICE}"
-    (docker-compose up $1 | tee $TEMPFILE) >> ${OUT} 2>&1 &
+    (docker compose up $1 | tee $TEMPFILE) >> ${OUT} 2>&1 &
     until grep "$WAIT_FOR" $TEMPFILE; do
         echo -n "."
         sleep 1
@@ -83,7 +84,7 @@ start_infra () {
 }
 
 init () {
-    echo "Creating network gob-network"
+    echo -e "\nCreating network gob-network"
     docker network create gob-network
 
     echo "Creating volume gob-volume"
@@ -91,7 +92,7 @@ init () {
     mkdir -m 777 -p $GOB_VOLUME_DIR/message_broker
     docker volume create gob-volume --opt device=$GOB_VOLUME_DIR --opt o=bind --opt type=tmpfs > /dev/null
 
-    echo "Init .env for relevant projects"
+    echo "Init .env for relevant compose projects"
     for REPO_DIR in $(find ./ -name ".env.example" -maxdepth 2 -type f | xargs -I{} dirname {})
     do
         cd $REPO_DIR
@@ -112,22 +113,18 @@ start () {
 
     # Initialize GOB, ignore errors
     init 2> /dev/null || true
-
-    # build "local" frontend docker
-    export NPMSCRIPT=builddev
+    echo -e "${BOLD_BLACK}Output${NC} in ${OUT}\n"
 
     export GOBOPTIONS=
     for REPO in ${BASE_REPOS} ${REPOS}
     do
-        echo "${GREEN}${REPO}${NC}"
-
         GOB_REPO="GOB-${REPO}"
 
         # Initialize each repository
         cd ${GOB_REPO}
 
             if [ "$REPO" = "Infra" ]; then
-                echo "Starting GOB infrastructure"
+                echo "Starting ${BOLD_BLACK}GOB Infrastructure${NC}"
                 for SERVICE in database management_database analyse_database; do
                     start_infra $SERVICE "database system is ready to accept connections"
                 done
@@ -135,9 +132,9 @@ start () {
             elif [ "$REPO" = "Core" ]; then
                 CORE_VERSION=$(grep "GOB-Core" requirements.txt | sed -E "s/^.*@(v.*)#.*$/\1/")
                 CURRENT_CORE_VERSION=$(git describe --abbrev=0 --tags)
-                echo "GOB Core Version: ${CURRENT_CORE_VERSION}"
+                echo "${BOLD_BLACK}GOB Core${NC} Version: ${GREEN}${CURRENT_CORE_VERSION}${NC}"
             else
-                echo "Starting docker GOB ${REPO}"
+                echo "Starting ${BOLD_BLACK}GOB ${REPO}${NC}"
                 if [ -f src/requirements.txt ]; then
                     CORE_VERSION=$(grep "GOB-Core" src/requirements.txt | sed -E "s/^.*@(v.*)#.*$/\1/")
                     echo -n "Core version"
@@ -148,10 +145,10 @@ start () {
                     fi
                     echo " ${CORE_VERSION} ${NC}"
                 fi
-                echo "Building ${REPO} docker"
-                docker-compose build > /dev/null
-                echo "Starting ${REPO} docker"
-                docker-compose up >> ${OUT} 2>&1 &
+                echo "Building ${GOB_REPO} compose project"
+                docker compose build > /dev/null
+                echo "Starting ${GOB_REPO} compose project"
+                docker compose up >> ${OUT} 2>&1 &
             fi
 
         cd ..
@@ -173,13 +170,16 @@ else
 fi
 
 if [ "$1" == "start" ]; then
-    stop_dockers
+    stop_projects
     start
     sleep 10
-    list_dockers
-    echo -e "\nOutput in ${OUT}${NC}"
+    echo
+    show_projects
 elif [ "$1" == "ls" ]; then
-    list_dockers
+    show_projects
 elif [ "$1" == "stop" ]; then
-    stop_dockers
+    stop_projects
+else
+  echo "Invalid parameters. Start with ${SCRIPT_PATH} (--force) [start|stop|ls]"
+  exit 1
 fi
